6d5b00291ad41d62843bd4286f3c697905bc5fad
SYJ1029 <abg0986@gmail.com>
2024-11-18 03:37:41 +0900

    bb의 크기도 줄여보자

--- mygame/Ball.py
+++ mygame/Ball.py
@@ -4,9 +4,12 @@ from gfw import *
 
 
 class Ball(Sprite):
+    ballwidth = 50
+    ballheight = 50
 
     def __init__(self):
         super().__init__(f'res/Ball.png',  640, 360 - 50)
+
     
     def handle_event(e):
         pass
@@ -15,8 +18,8 @@ class Ball(Sprite):
         pass
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        self.image.draw(self.x, self.y, self.ballwidth, self.ballheight)
 
     def get_bb(self):
-        return self.x - 30, self.y - 32, self.x + 30, self.y + 28
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
 
