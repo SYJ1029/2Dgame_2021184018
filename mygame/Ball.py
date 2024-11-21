c1d9bf2d750705149571a82696adf48d67e29a7a
DESKTOP-HGMKATH\abg09 <abg0986@gmail.com>
2024-11-21 21:52:58 +0900

    bb가 점점 커지다가 원래 크기에 접어들면 야구공을 사라지게 하자

--- mygame/Ball.py
+++ mygame/Ball.py
@@ -1,14 +1,18 @@
 from pico2d import * 
 from gfw import *
+import copy
 
 
 
 class Ball(Sprite):
     ballwidth = 40
     ballheight = 40
-    area = 0
+    area = 20
+    initpos = [0, 0]
     maxarea = 40 * 40
-    center = [650, 200]
+    center = [610, 240]
+    inited = False
+    godraw = False
 
 
     def __init__(self, ballarea):
@@ -16,21 +20,44 @@ class Ball(Sprite):
         self.center += ballarea
         self.ballwidth = 40
         self.ballheight = 40
-        self.area = 0
+        self.initpos = copy.deepcopy(self.center)
+        self.inited = False
+ 
 
     def initbbpos():
         pass
     
-    def handle_event(e):
-        pass
+    def handle_event(self, e):
+        if e.type == SDL_KEYDOWN:
+            if e.key == SDLK_RETURN:
+                print('It works!')
+                print()
+            if e.key >= SDLK_1 and e.key <= SDLK_9:
+                token = e.key - SDLK_0 - 1
+                row = token // 3
+                column = token % 3
+                print(row, column)
+
+                self.center[0] = self.initpos[0] + column * self.ballwidth
+                self.center[1] = self.initpos[1] - row * self.ballheight
+
+                pass
+
+
 
     def update(self):
-        self.area += 1
+        if(self.godraw):
+            self.area -= 0.1
+        
+        if(self.area <= 0):
+            self.godraw = False
+        #print(self.area)
 
     def draw(self):
-        self.image.draw(self.x, self.y, self.ballwidth, self.ballheight)
+        if self.godraw:
+            self.image.draw(self.x, self.y, self.ballwidth, self.ballheight)
 
     def get_bb(self):
-        return (self.center[0] - self.ballwidth / 2, self.center[1] - self.ballheight / 2, 
-                self.center[0] + self.ballwidth / 2, self.center[1] + self.ballheight / 2)
+        return (self.center[0] - (self.ballwidth / 2 - self.area), self.center[1] - (self.ballheight / 2 - self.area), 
+                self.center[0] + (self.ballwidth / 2 - self.area), self.center[1] + (self.ballheight / 2 - self.area))
 
