a8023e3bcac2202eb3fec5d34bd72d260de3800c
SYJ1029 <abg0986@gmail.com>
2024-11-21 23:08:08 +0900

    공과 타자가 정상적으로 면적을 출력해주고 있다.

--- mygame/Batter.py
+++ mygame/Batter.py
@@ -1,6 +1,6 @@
 from pico2d import * 
 from gfw import *
-
+from Ball import Ball
 
 class Batter(Sprite):
 
@@ -11,7 +11,7 @@ class Batter(Sprite):
         self.batheight = 40
 
 
-    def handle_event(self, e):
+    def handle_event(self, e, ball):
         if e.type == SDL_KEYDOWN:
             pass
         if e.type == SDL_MOUSEBUTTONDOWN:
@@ -19,6 +19,7 @@ class Batter(Sprite):
                 self.center[0] = e.x
                 self.center[1] = 720 - e.y
                 print(self.center)
+                self.Check_collision(ball)
 
 
     def update(self):
@@ -30,3 +31,33 @@ class Batter(Sprite):
     def get_bb(self):
         return (self.center[0] - self.batwidth / 2, self.center[1] - self.batheight / 2, 
             self.center[0] + self.batwidth / 2, self.center[1] + self.batheight / 2)
+    
+
+    def GetOverlapBox(self, ball):
+        batterbb = [[self.center[0] - self.batwidth / 2, self.center[1] - self.batheight / 2], 
+                [self.center[0] + self.batwidth / 2, self.center[1] + self.batheight / 2]]
+        print(f'Batterbb: {list(batterbb)}\n')
+
+        ballbb = [[ball.center[0] - ball.ballwidth / 2, ball.center[1] - ball.ballwidth / 2], 
+                [ball.center[0] + ball.ballwidth / 2, ball.center[1] + ball.ballwidth / 2]]
+        print(f'Ballbb: {list(ballbb)}\n')
+
+        overlap1 = [max(batterbb[0][0], ballbb[0][0]), max(batterbb[0][1],ballbb[0][1])]
+        overlap2 = [min(batterbb[1][0], ballbb[1][0]), min(batterbb[1][1], ballbb[1][1])]
+        print(f'overlapbb: {list(overlap1)}, {list(overlap2)}')
+
+        if overlap1[0] < overlap2[0] and overlap1[1] < overlap2[1]:
+            return [overlap1, overlap2]
+        else:
+            return None  # 겹치는 영역 없음
+
+    def Check_collision(self, ball):
+        overlap = self.GetOverlapBox(ball)
+
+        if overlap != None:
+            area = (overlap[1][0] - overlap[0][0]) * (overlap[1][1] - overlap[0][1])
+            print(area)
+            return area
+        
+        return None
+   
