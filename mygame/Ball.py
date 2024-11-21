d9e601f9cc153729129b38cf31e1356b6715fe41
DESKTOP-HGMKATH\abg09 <abg0986@gmail.com>
2024-11-21 20:59:03 +0900

    enter키를 누르면 공이 생성되게

--- mygame/Ball.py
+++ mygame/Ball.py
@@ -4,18 +4,27 @@ from gfw import *
 
 
 class Ball(Sprite):
-    ballwidth = 50
-    ballheight = 50
+    ballwidth = 40
+    ballheight = 40
+    area = 0
+    maxarea = 40 * 40
+    center = 0
 
-    def __init__(self):
+
+    def __init__(self, ballarea):
         super().__init__(f'res/Ball.png',  600, 360 - 50)
+        self.center = ballarea
+        self.ballwidth = 40
+        self.ballheight = 40
+        self.area = 0
+
 
     
     def handle_event(e):
         pass
 
     def update(self):
-        pass
+        self.area += 1
 
     def draw(self):
         self.image.draw(self.x, self.y, self.ballwidth, self.ballheight)
