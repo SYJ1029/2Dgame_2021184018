c60fdef03562e90d714516379eaaba56b34f0b7b
SYJ1029 <abg0986@gmail.com>
2024-11-29 11:54:59 +0900

    여러 수정사항 반영
    1. Defending,py는 이제 2개의 팀으로 나뉘고, 이들은 수비수가 된다
    - 수비수들을 렌더링 했다.
    > 다만 이미지는 투수 이미지를 덮어씌웠고, 아직 위치를 명확하게 해주진 않았다.
    
    2. Player.py, Defending.py, Staduim_2.png 추가
    - 3개의 파일을 추가했다. 안되어있더라

---
+++ mygame/Player.py
@@ -0,0 +1,18 @@
+from pico2d import * 
+from gfw import *
+from Ball import Ball
+
+
+class Player(Sprite):
+	def __init__(self, filename, x, y):
+
+		super().__init__(filename,  x, y)
+
+	def handle_event(e):
+		pass
+
+	def update(self):
+		pass
+
+	def draw(self):
+		self.image.draw(self.x, self.y)
