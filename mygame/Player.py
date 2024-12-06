ac697783b448b28d1bb4bd44c6dcf4d4d90332e9
LAPTOP-KAKG2S7N\abg09 <abg0986@gmail.com>
2024-12-06 11:05:20 +0900

    공의 변화량을 수비수들의 위치에 빼서 수비가 제 위치를 유지하는 상대적 위치로 전환을 시도해보았다. 시도는 좋았지만 값이 너무 작아서 제대로 위치를 유지하지 못한다.

--- mygame/Player.py
+++ mygame/Player.py
@@ -1,17 +1,24 @@
 from pico2d import * 
 from gfw import *
 from Ball import *
+import copy
 
 
 class Player(Sprite):
-	def __init__(self, filename, x, y):
+	def __init__(self, filename, x, y, ball):
 		super().__init__(filename,  x, y)
+		self.initx = copy.deepcopy(self.x)
+		self.inity = copy.deepcopy(self.y)
+		self.ball = ball
+		self.dx = 0
+		self.dy = 0
 
 	def handle_event(e):
 		pass
 
 	def update(self):
-		pass
+		self.x = self.initx - (self.ball.x - self.ball.initxy[0])
+		self.y = self.inity - (self.ball.y - self.ball.initxy[1])
 
 	def draw(self):
 		self.image.draw(self.x, self.y)
